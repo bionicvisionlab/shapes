edd9f2d5c9a4878efabe2ae9c3110801acc1eaba
Jacob Granley <jgranley@ucsb.edu>
2021-05-20 12:01:02 -0700

    Added function to save shapes to h5

--- shapes/shapes.py
+++ shapes/shapes.py
@@ -185,3 +185,75 @@ def load_shapes(h5file, subjects=None, stim_class=['SingleElectrode', 'MultiElec
     if shuffle:
         df = df.sample(n=len(df), random_state=random_state)
     return df.reset_index(drop=True)
+
+
+def average_trials(df):
+    """
+    Takes in a dataframe, and contains logic for aggregating the different fields across trials. 
+    For example, some fields like 'image' and 'num_regions' should just be averaged, some fields
+    like 'amp' and 'freq' should be constant, some fields like 'filename' no longer make sense,
+    and some fields such as regionprops measurements should (maybe) be recalculated on the averaged images
+    """
+    # TODO
+    pass
+
+
+def save_shapes(df, h5_file, ignore_overwrite=False):
+    """
+    Saves df to h5 file.
+    Note that the h5 file will be completely overwritten to the new dataframe,
+    so make sure that this new dataframe still contains all of the data you wish to save
+    """
+    if not ignore_overwrite and len([s for s in subject_params.keys() if s not in df['subject'].unique()]) > 0:
+        raise ValueError("Missing subjects in new dataframe, use with ignore_overwrite=True to force save")
+
+    stim_types = ['CDL0.35', 'CDL0.75', 'MultiElectrode', 'SingleElectrode', 'SpatialSummation',
+                  'Step1', 'Step1a', 'Step1b', 'Step1c', 'Step1d', 'Step2&3', 'Step2a', 'Step3',
+                  'Step4', 'Step5', 'Step6a']
+    if not ignore_overwrite and len([s for s in stim_types if s not in df['stim_class'].unique()]) > 0:
+        raise ValueError("Missing stim_class in new dataframe, use with ignore_overwrite=True to force save")
+
+    
+    """Converts the data from Pandas DataFrame to HDF5"""
+    Xy = df.copy()
+
+    Xy['image'] = pd.Series([row.image for (_, row) in Xy.iterrows()], index=Xy.index)
+
+    # Convert images into black and white: (already black and white)
+    Xy.image = Xy.image.apply(lambda x: x.astype(np.bool))
+    
+    # Split 'img_shape' into 'img_shape_x' and 'img_shape_y':
+    Xy['img_shape_x'] = Xy.img_shape.apply(lambda x: x[0])
+    Xy['img_shape_y'] = Xy.img_shape.apply(lambda x: x[1])
+    Xy.drop(columns='img_shape', inplace=True)
+    
+        
+    file = h5py.File(h5_file, 'w')
+    # split by subject so each can be loaded individually
+    for subject, data in Xy.groupby('subject'):
+        # Image data:
+        file.create_dataset("%s.image" % subject,
+                            data=np.array([row.image
+                                           for (_, row) in data.iterrows()]),
+                           dtype=np.bool_)
+        # String data:
+        for col in ['subject', 'filename', 'stim_class', 'electrode1', 'electrode2', 'implant', 'date']:
+            dt = h5py.string_dtype(encoding='utf-8')
+            file.create_dataset("%s.%s" % (subject, col),
+                                data=np.array([row[col]
+                                               for (_, row) in data.iterrows()],
+                                              dtype=dt))
+        # Int data:
+        for col in ['area', 'img_shape_x', 'img_shape_y', 'experiment', 'trial', 'num_regions']:
+            file.create_dataset("%s.%s" % (subject, col),
+                                data=np.array([row[col]
+                                               for (_, row) in data.iterrows()],
+                                              dtype=np.int32))
+        # Float data:
+        for col in ['amp1', 'amp2', 'freq', 'pdur', 'x_center', 'y_center', 'orientation',
+                    'eccentricity', 'compactness']:
+            file.create_dataset("%s.%s" % (subject, col),
+                                data=np.array([row[col]
+                                               for (_, row) in data.iterrows()],
+                                              dtype=np.float32))
+    file.close()
